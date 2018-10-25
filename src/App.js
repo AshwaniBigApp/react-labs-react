c84a5100f45b55107a68c0a55d66322841cdc5ad
AshwaniBigApp <ashwani@bigappcompany.in>
2018-10-25 19:13:49 +0530

    react labs

--- src/App.js
+++ src/App.js
@@ -1,28 +1,73 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor(){
+    super();
+    this.state = {
+      data:
+      [
+        {
+          "id":1,
+          "name":"Ashwani",
+          "age":"20"
+        }
+      ]
+    }
+  }
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div>
+        <Header />
+        <table class="table">
+          <thead>
+            <tr>
+              <th>Sl. No.</th>
+              <th>Name</th>
+              <th>Age</th>
+            </tr>
+          </thead>
+          <tbody>
+            {
+              this.state.data.map(
+                (person, i) => <TableRow key = {i} data = {person} />            
+              )
+            }
+          </tbody>
+        </table>
+        <Content />
       </div>
     );
   }
 }
+class TableRow extends React.Component{
+  render(){
+    return(
+      <tr>
+        <td>{this.props.data.id}</td>
+        <td>{this.props.data.name}</td>
+        <td>{this.props.data.age}</td>
+      </tr>
+    )
+  }
+}
+class Header extends React.Component{
+  render(){
+    return(
+      <div>
+        Header WOrks
+      </div>
+    )
+  }
+}
 
+class Content extends React.Component{
+  render(){
+    return(
+      <div>
+        Content Works
+      </div>
+    )
+  }
+}
 export default App;
